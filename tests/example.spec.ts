58a681afbcb9d197b99170a839632967c8f7d3e9
shankeyrahul15 <shankeyrahul15@gmail.com>
2025-10-16 20:22:48 +0530

    updated again

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Updated from GitHub has title', async ({ page }) => {
+test('Updated again from GitHub has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
